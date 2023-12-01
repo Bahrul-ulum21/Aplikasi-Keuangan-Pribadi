e13aef205d21854f01971f2ed5c8d87b9b7d5839
Raqanzaa <142374468+Raqanzaa@users.noreply.github.com>
2023-12-01 10:29:47 +0700

    menambahkan beberapa penyesuaian

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -17,43 +17,43 @@
       </nav>
     </div>
     <div class="row">
-      <div class="col-md-4 stretch-card grid-margin">
-        <div class="card bg-gradient-danger card-img-holder text-white">
+      <div class="col-md-3 stretch-card grid-margin">
+        <div class="card bg-gradient-dark card-img-holder text-white">
           <div class="card-body">
             <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-            <h4 class="font-weight-normal mb-3">Semua Saldo<i class="mdi mdi-chart-line mdi-24px float-right"></i>
+            <h4 class="font-weight-normal mb-3">Semua Saldo
             </h4>
-            <h2 class="mb-5">Rp. 15,000.000</h2>
+            <h3 class="mb-5">Rp. 15,000,000</h3>
           </div>
         </div>
       </div>
-      <div class="col-md-4 stretch-card grid-margin">
+      <div class="col-md-3 stretch-card grid-margin">
         <div class="card bg-gradient-info card-img-holder text-white">
           <div class="card-body">
             <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-            <h4 class="font-weight-normal mb-3">Saldo Bulan Ini <i class="mdi mdi-bookmark-outline mdi-24px float-right"></i>
+            <h4 class="font-weight-normal mb-3">Saldo Bulan Ini 
             </h4>
-            <h2 class="mb-5">Rp. 100.000.000</h2>
+            <h3 class="mb-5">Rp. 100,000,000</h3>
           </div>
         </div>
       </div>
-      <div class="col-md-4 stretch-card grid-margin">
-        <div class="card bg-gradient-success card-img-holder text-white">
+      <div class="col-md-3 stretch-card grid-margin">
+        <div class="card bg-gradient-danger card-img-holder text-white">
           <div class="card-body">
             <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-            <h4 class="font-weight-normal mb-3">Pengeluaran Bulan Ini <i class="mdi mdi-diamond mdi-24px float-right"></i>
+            <h4 class="font-weight-normal mb-3">Pengeluaran Bulan Ini 
             </h4>
-            <h2 class="mb-5">Rp. 30.000.000</h2>
+            <h3 class="mb-5">Rp. 30,000,000</h3>
           </div>
         </div>
       </div>
-      <div class="col-md-4 stretch-card grid-margin">
+      <div class="col-md-3 stretch-card grid-margin">
         <div class="card bg-gradient-success card-img-holder text-white">
           <div class="card-body">
             <img src="assets/images/dashboard/circle.svg" class="card-img-absolute" alt="circle-image" />
-            <h4 class="font-weight-normal mb-3">Pemasukan Bulan Ini <i class="mdi mdi-diamond mdi-24px float-right"></i>
+            <h4 class="font-weight-normal mb-3">Pemasukan Bulan Ini 
             </h4>
-            <h2 class="mb-5">Rp. 300.000.000</h2>
+            <h3 class="mb-5">Rp. 300,000,000</h3>
           </div>
         </div>
       </div>
--- resources/views/layouts/sidebar.blade.php
+++ resources/views/layouts/sidebar.blade.php
@@ -4,13 +4,19 @@
                         <div class="nav-profile-image"><img src="assets/images/faces/face1.jpg" alt="profile"><span
                                 class="login-status online"></span>
                         </div>
-                        <div class="nav-profile-text d-flex flex-column"><span class="font-weight-bold mb-2">{{ Auth::user()->name }}</span><span class="text-secondary text-small">{{ Auth::user()->email  }}</span></div><i
-                            class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
+                        <div class="nav-profile-text d-flex flex-column">
+                            <span class="font-weight-bold mb-2 profile-name">
+                                {{ Auth::user()->name }}
+                            </span>
+                            <span class="text-secondary text-small">{{ Auth::user()->email }}</span>
+                        </div>
+                        
+                        <i class="mdi mdi-bookmark-check text-success nav-profile-badge"></i>
                     </a></li>
                 <li class="nav-item"><a class="nav-link" href="/home"><span class="menu-title">Dashboard</span><i
                             class="mdi mdi-home menu-icon"></i></a></li>
                 <li class="nav-item">  <a class="nav-link" data-bs-toggle="collapse" href="#ui-basic"
-                        aria-expanded="false" aria-controls="ui-basic"><span class="menu-title">KATEGORI</span><i
+                        aria-expanded="false" aria-controls="ui-basic"><span class="menu-title">Kategori</span><i
                             class="menu-arrow"></i><i class="mdi mdi-server-security"></i></a>
                     <div class="collapse" id="ui-basic">
                         <ul class="nav flex-column sub-menu">
@@ -20,7 +26,7 @@
                     </div>
                 </li>
                 <li class="nav-item"><a class="nav-link" data-bs-toggle="collapse" href="#ui-basic1"
-                    aria-expanded="false" aria-controls="ui-basic"><span class="menu-title">TRANSAKSI</span><i
+                    aria-expanded="false" aria-controls="ui-basic"><span class="menu-title">Transaksi</span><i
                         class="menu-arrow"></i><i class="mdi mdi-cash-multiple"></i></a>
                 <div class="collapse" id="ui-basic1">
                     <ul class="nav flex-column sub-menu">
@@ -29,8 +35,16 @@
                     </ul>
                 </div>
             </li>
-                <li class="nav-item"><a class="nav-link" data-bs-toggle="collapse" href="#ui-basic"aria-expanded="false" aria-controls="ui-basic"><span class="menu-title">LAPORAN</span>
+                <li class="nav-item"><a class="nav-link" data-bs-toggle="collapse" href="#ui-basic"aria-expanded="false" aria-controls="ui-basic"><span class="menu-title">Laporan</span>
                     <i class="menu-arrow"></i>
                     <i class="mdi mdi-chart-areaspline"></i></a></li>
             </ul>
         </nav>
+<style>
+    .profile-name {
+    white-space: nowrap;
+    overflow: hidden;
+    text-overflow: ellipsis;
+    max-width: 150px;
+}
+</style>
