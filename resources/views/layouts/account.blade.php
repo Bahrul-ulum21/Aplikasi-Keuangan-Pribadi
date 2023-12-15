3ac3d3ec51f2f6e9ddceec454c41945190b89f73
ACER <rizmasabillah02@gmail.com>
2023-12-15 12:52:45 +0700

    ubah logo dan nama

--- resources/views/layouts/account.blade.php
+++ resources/views/layouts/account.blade.php
@@ -5,7 +5,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    <title>Purple Admin</title>
+    <title>E-money</title>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     <!-- plugins:css -->
     <link rel="stylesheet" href="{{asset('assets/vendors/mdi/css/materialdesignicons.min.css')}}">
